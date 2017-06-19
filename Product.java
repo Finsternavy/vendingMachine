fac9ee5eb1d5eea311d99c3f7610eea8e49884d3
CFINPC\Finst <finsternavy@gmail.com>
2017-06-18 22:13:23 -0700

    Week 3 Class updates

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 package vendingMachine;
 
-public class Product {
+public class Product implements Comparable<Product>{
 	
 	  String name = "New Product"; 
 	  double price = 1.00; 
@@ -53,5 +53,20 @@ public class Product {
 	    public String toString(){
 	    	return name + " Price: $" + price;
 	    }
+	    
+		@Override
+		public int compareTo(Product other) {
+			if(getName().compareToIgnoreCase(other.getName()) > 0){
+				return 1;
+			}else if(getName().compareToIgnoreCase(other.getName()) < 0){
+				return -1;
+			}else if(getPrice() > other.getPrice()){
+				return 1;
+			}else if(getPrice() < other.getPrice()){
+				return -1;
+			}else{
+				return 0;
+			}
+		}
 	
 }
--- Snack.java
+++ Snack.java
@@ -1,6 +1,6 @@
 package vendingMachine;
 
-public abstract class Snack extends Product{
+public abstract class Snack extends Product implements Comparable<Product>{
 	
     public Snack () {
         super();
@@ -14,5 +14,24 @@ public abstract class Snack extends Product{
     	this.maxStock = maxQuantity;
 
     }
-
+    
+	@Override
+	public String toString(){
+		return "\n" + name + ":\nPrice: " + price;
+	}
+    
+	@Override
+	public int compareTo(Product other) {
+		if(getName().compareToIgnoreCase(other.getName()) > 0){
+			return 1;
+		}else if(getName().compareToIgnoreCase(other.getName()) < 0){
+			return -1;
+		}else if(getPrice() > other.getPrice()){
+			return 1;
+		}else if(getPrice() < other.getPrice()){
+			return -1;
+		}else{
+			return 0;
+		}
+	}
 }
