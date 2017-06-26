d85afd6b8b782358cb5d0de23b403c814231aeb7
corvy17 <i.lopez.34.118@gmail.com>
2017-06-25 21:03:36 -0700

    Uploaded images
    I added the images to the corresponding slots

--- Main.java
+++ Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		
 		BorderPane pane = new BorderPane();
 		
 		HBox hbox = getHBox();
@@ -56,52 +57,84 @@ public class Main extends Application {
 		category.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
 		grid.add(category, 4, 0);
 		
-		ImageView imageDorito = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageDorito = new ImageView(new Image("https://vignette1.wikia.nocookie.net/plantsvszombies/images/c/ce/Doritos-nacho-cheese.png/revision/latest?cb=20150624130919"));
+		imageDorito.setFitHeight(150);
+		imageDorito.setFitWidth(100);
 		grid.add(imageDorito, 1, 1, 2, 1);
 		
-		ImageView imageLay = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageLay = new ImageView(new Image("https://vignette4.wikia.nocookie.net/fantendo/images/4/48/Lays_Chips.png/revision/latest?cb=20140225153527"));
+		imageLay.setFitHeight(150);
+		imageLay.setFitWidth(100);
 		grid.add(imageLay, 3, 1, 2, 1);
 
-		ImageView imageSunChip = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageSunChip = new ImageView(new Image("http://www.fritolay.com/images/default-source/blue-bag-image/sunchips-harvest-cheddar772e41e0cc54631c95cbff00000d71c6.png?sfvrsn=2"));
+		imageSunChip.setFitHeight(150);
+		imageSunChip.setFitWidth(100);
 		grid.add(imageSunChip, 5, 1, 2, 1);
 		
-		ImageView imageMissVickie = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageMissVickie = new ImageView(new Image("http://www.fritolay.com/images/default-source/blue-bag-image/miss-vickies-jalapeno.png?sfvrsn=4"));
+		imageMissVickie.setFitHeight(150);
+		imageMissVickie.setFitWidth(100);
 		grid.add(imageMissVickie, 7, 1, 2, 1);
 		
-		ImageView imageCoke = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageCoke = new ImageView(new Image("http://www.freepngimg.com/thumb/coca%20cola/15-coca-cola-can-png-image-thumb.png"));
+		imageCoke.setFitHeight(150);
+		imageCoke.setFitWidth(100);
 		grid.add(imageCoke, 1, 2, 2, 1);
 		
-		ImageView imageSprite = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageSprite = new ImageView(new Image("http://3.bp.blogspot.com/-qyKvkc8tP1Q/T-6qacpvZDI/AAAAAAAABOs/rr6uurTdgd0/s1600/sprite.jpg"));
+		imageSprite.setFitHeight(150);
+		imageSprite.setFitWidth(100);
 		grid.add(imageSprite, 3, 2, 2, 1);
 		
-		ImageView imageJuice = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageJuice = new ImageView(new Image("http://www.bemidjicoke.com/_assets/img/juice-tea/minute%20maid.png"));
+		imageJuice.setFitHeight(150);
+		imageJuice.setFitWidth(100);
 		grid.add(imageJuice, 5, 2, 2, 1);
 		
-		ImageView imageWater = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageWater = new ImageView(new Image("http://www.coca-colaproductfacts.com/content/dam/productfacts/us/productDetails/ProductImages/PDP_DASANI_1L_bottle.png"));
+		imageWater.setFitHeight(150);
+		imageWater.setFitWidth(100);
 		grid.add(imageWater, 7, 2, 2, 1);
 		
-		ImageView imageMM = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageMM = new ImageView(new Image("https://target.scene7.com/is/image/Target/13055555?wid=520&hei=520&fmt=pjpeg"));
+		imageMM.setFitHeight(75);
+		imageMM.setFitWidth(100);
 		grid.add(imageMM, 1, 3, 2, 1);
 		
-		ImageView imageStarburst = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageStarburst = new ImageView(new Image("http://prod-cdn.thekrazycouponlady.com/wp-content/uploads/2012/10/Screen-Shot-2012-10-25-at-12.36.17-PM.png"));
+		imageStarburst.setFitHeight(75);
+		imageStarburst.setFitWidth(100);
 		grid.add(imageStarburst, 3, 3, 2, 1);
 		
-		ImageView imageSkittles = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageSkittles = new ImageView(new Image("http://florencemarkoferpto.org/wp-content/uploads/2016/08/skittles.png"));
+		imageSkittles.setFitHeight(75);
+		imageSkittles.setFitWidth(100);
 		grid.add(imageSkittles, 5, 3, 2, 1);
 		
-		ImageView imageSnicker = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageSnicker = new ImageView(new Image("http://vignette3.wikia.nocookie.net/officialinanimations/images/a/a8/Snickers.png/revision/latest?cb=20140418143336"));
+		imageSnicker.setFitHeight(50);
+		imageSnicker.setFitWidth(150);
 		grid.add(imageSnicker, 7, 3, 2, 1);
 		
-		ImageView imageTrident= new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageTrident= new ImageView(new Image("https://cdn.shopify.com/s/files/1/0972/7116/products/trident-tropical-twist-18-stick-pack.png?v=1459344968"));
+		imageTrident.setFitHeight(75);
+		imageTrident.setFitWidth(75);
 		grid.add(imageTrident, 1, 4, 2, 1);
 		
-		ImageView imageExtra = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageExtra = new ImageView(new Image("http://www.extragum.com/img/site/spearmint_bigpack2.png"));
+		imageExtra.setFitHeight(50);
+		imageExtra.setFitWidth(75);
 		grid.add(imageExtra, 3, 4, 2, 1);
 		
-		ImageView imageMint = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageMint = new ImageView(new Image("https://s3.amazonaws.com/user-media.venngage.com/504792-167414292e19e303c90e4bd7a8532a5d.png"));
+		imageMint.setFitHeight(75);
+		imageMint.setFitWidth(75);
 		grid.add(imageMint, 5, 4, 2, 1);
 		
-		ImageView imageRed = new ImageView(new Image("https://vignette3.wikia.nocookie.net/doritos/images/6/62/Doritos-nacho-cheese.gif/revision/latest?cb=20130727212532"));
+		ImageView imageRed = new ImageView(new Image("https://www.sciencenews.org/sites/default/files/4096"));
+		imageRed.setFitHeight(50);
+		imageRed.setFitWidth(75);
 		grid.add(imageRed, 7, 4, 2, 1);
 		
 		return grid;
